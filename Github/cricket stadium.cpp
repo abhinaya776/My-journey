7ad5a3a7528b675b94b8cbefc1e6bd47442997c7
abhinaya776 <abhinayanandhini07@gmail.com>
2025-06-19 17:56:29 +0530

    C++  snippets

---
+++ Github/cricket stadium.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num1,num2;
+    cout<<"";
+    cin>>num1;
+    cout<<"";
+    cin>>num2;
+    cout<<2*(num1+num2)<<endl;
+    cout<<num1*num2;
+    return 0;
+}
