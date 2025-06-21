4f7e366b7d138786111adc9d485910cdab60275d
Abhinaya S M <abhinayanandhini07@gmail.com>
2025-06-21 19:32:37 +0530

    1 2 4 7 11 16 series using for loop.cpp

---
+++ 1 2 4 7 11 16 series using for loop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int j=1;
+    for(int i=0;i<n;i++){
+        j+=i;
+       cout<<j<<" ";
+      }
+    return 0;
+}
