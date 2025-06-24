2e599b45e485c180946566c6b55db0b725cd46b1
Abhinaya S M <abhinayanandhini07@gmail.com>
2025-06-24 12:43:53 +0530

    sum of elements using 2D array.cpp

---
+++ sum of elements using 2D array.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n; cin>>n;
+    int arr[n][n];
+    int sum=0;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>arr[i][j];
+    }}
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+          sum +=arr[i][j];
+
+            }
+        cout<<endl;
+        
+    }
+cout<<sum<<endl;
+
+    return 0;
+}
